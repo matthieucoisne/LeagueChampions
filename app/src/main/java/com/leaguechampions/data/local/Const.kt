e7bab9849ac810ffbe488abc425b845b53e91b09
Matthieu Coisne <matthieu.coisne@gmail.com>
2017-12-11 22:09:11 -0500

    migration to kotlin: Const.kt

---
+++ app/src/main/java/com/leaguechampions/data/local/Const.kt
@@ -0,0 +1,15 @@
+package com.leaguechampions.data.local
+
+object Const {
+
+    const val URL = "http://ddragon.leagueoflegends.com/cdn/7.15.1/data/en_US/"
+
+    const val KEY_CHAMPION_ID = "com.leaguechampions.championId"
+    const val KEY_VERSION = "com.leaguechampions.version"
+
+
+    // DEBUG
+    const val BEHAVIOR_DELAY_MILLIS: Long = 0
+    const val PREF_KEY_MOCK_MODE = "mock_mode"
+    const val isGlide = false
+}
