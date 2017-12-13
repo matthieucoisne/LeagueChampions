b831845ce4555c48b4ba71ee46aead9aebc0eb3f
Matthieu Coisne <matthieu.coisne@gmail.com>
2017-12-13 17:27:55 -0500

    migration to kotlin: NetworkModule.kt

---
+++ app/src/main/java/com/leaguechampions/injection/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.leaguechampions.injection
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    internal fun provideOkHttpClient(cache: Cache, loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+                .cache(cache)
+                .addInterceptor(loggingInterceptor)
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideOkHttpCache(context: Context): Cache {
+        val cacheSize = 10 * 1024 * 1024 // 10 MB
+        return Cache(context.cacheDir, cacheSize.toLong())
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+}
