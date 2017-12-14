a55daf4025c31dcd1b04a1cfe05005b0c4d7c0c8
Matthieu Coisne <matthieu.coisne@gmail.com>
2017-12-13 22:33:14 -0500

    migration to kotlin: SettingsActivityTest.kt

---
+++ app/src/androidTest/java/com/leaguechampions/ui/settings/SettingsActivityTest.kt
@@ -0,0 +1,39 @@
+package com.leaguechampions.ui.settings
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import com.leaguechampions.utils.PrefUtils
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SettingsActivityTest {
+
+    @Rule
+    var activityTestRule: ActivityTestRule<SettingsActivity> = object : ActivityTestRule<SettingsActivity>(SettingsActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            PrefUtils.setMockMode(InstrumentationRegistry.getTargetContext(), true)
+            super.beforeActivityLaunched()
+        }
+    }
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun checkVersionIsDisplayed() {
+        SettingsRobot()
+                .checkVersionIsDisplayed()
+    }
+}
