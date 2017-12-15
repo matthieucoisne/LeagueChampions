cd1380d79f83bbcb3672ce024354b3129c6ffc61
Matthieu Coisne <matthieu.coisne@gmail.com>
2017-12-14 22:53:46 -0500

    migration to kotlin: SettingsPresenterTest.kt

---
+++ app/src/test/java/com/leaguechampions/ui/settings/SettingsPresenterTest.kt
@@ -0,0 +1,102 @@
+package com.leaguechampions.ui.settings
+
+import android.content.SharedPreferences
+import android.view.MenuItem
+import com.leaguechampions.BuildConfig
+import com.leaguechampions.data.local.Const
+import com.leaguechampions.utils.PrefUtils
+import com.leaguechampions.utils.ReflectionUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(BuildConfig::class, PrefUtils::class)
+class SettingsPresenterTest {
+
+    private val fieldBuildType = "buildType"
+
+    private lateinit var presenter: SettingsPresenter
+
+    @Mock private lateinit var view: SettingsPresenter.SettingsView
+    @Mock private lateinit var sharedPreferences: SharedPreferences
+
+    @Before
+    fun setUp() {
+        presenter = SettingsPresenter(view, sharedPreferences)
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun testOnActivityCreated() {
+        val mockMode = true
+        PowerMockito.mockStatic(PrefUtils::class.java)
+        `when`(PrefUtils.isMockMode(sharedPreferences)).thenReturn(mockMode)
+        ReflectionUtils.setField(presenter, fieldBuildType, "debug")
+
+        presenter.onActivityCreated(null, null)
+
+        verify(view).setVersion(BuildConfig.VERSION_NAME)
+        verify(view).showDeveloperOptions()
+        verify(view).showMockMode(mockMode)
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun testOnActivityCreated_WhenBuildTypeRelease_OnlySetsVersion() {
+        PowerMockito.mockStatic(PrefUtils::class.java)
+        ReflectionUtils.setField(presenter, fieldBuildType, "release")
+
+        presenter.onActivityCreated(null, null)
+
+        verify(view).setVersion(BuildConfig.VERSION_NAME)
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun testOnOptionsItemSelected() {
+        val item = mock(MenuItem::class.java)
+        `when`(item.itemId).thenReturn(android.R.id.home)
+
+        val result = presenter.onOptionsItemSelected(item)
+
+        assertThat(result).isEqualTo(true)
+        verify(view).doFinish()
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun testOnOptionsItemSelected_WhenItemNotHome_DoNothing() {
+        val item = mock(MenuItem::class.java)
+        `when`(item.itemId).thenReturn(0)
+
+        val result = presenter.onOptionsItemSelected(item)
+
+        assertThat(result).isEqualTo(false)
+        verifyNoMoreInteractions(view)
+    }
+
+    @Test
+    fun testOnMockModeCheckedChanged() {
+        val mockMode = true
+        val editor = mock(SharedPreferences.Editor::class.java)
+        PowerMockito.mockStatic(PrefUtils::class.java)
+        `when`(sharedPreferences.edit()).thenReturn(editor)
+        `when`(editor.putBoolean(Const.PREF_KEY_MOCK_MODE, mockMode)).thenReturn(editor)
+
+        presenter.onMockModeCheckedChanged(mockMode)
+
+        verifyNoMoreInteractions(view)
+    }
+}
