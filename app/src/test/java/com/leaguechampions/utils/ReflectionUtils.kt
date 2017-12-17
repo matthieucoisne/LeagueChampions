bf91162a354edfa022b96aa6c2155f88b5b9cdc6
Matthieu Coisne <matthieu.coisne@gmail.com>
2017-12-17 16:19:51 -0500

    migration to kotlin: ReflectionUtils.kt

---
+++ app/src/test/java/com/leaguechampions/utils/ReflectionUtils.kt
@@ -0,0 +1,35 @@
+package com.leaguechampions.utils
+
+import java.lang.reflect.Field
+
+object ReflectionUtils {
+
+    fun getField(clazz: Any, fieldName: String): Any {
+        val field = getDeclaredField(clazz.javaClass, fieldName)
+        field!!.isAccessible = true
+        return field.get(clazz)
+    }
+
+    fun setField(clazz: Any, fieldName: String, value: Any) {
+        val field = getDeclaredField(clazz.javaClass, fieldName)
+        field!!.isAccessible = true
+        field.set(clazz, value)
+    }
+
+    /**
+     * Recursive call
+     */
+    private fun getDeclaredField(clazz: Class<*>, fieldName: String): Field? {
+        var field: Field? = null
+        val superClass = clazz.superclass
+        if (superClass != null) {
+            field = getDeclaredField(superClass, fieldName)
+        }
+        if (field == null) {
+            try {
+                field = clazz.getDeclaredField(fieldName)
+            } catch (ignored: Exception) {}
+        }
+        return field
+    }
+}
