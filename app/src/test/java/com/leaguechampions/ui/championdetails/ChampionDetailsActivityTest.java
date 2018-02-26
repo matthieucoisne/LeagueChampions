8d0abe74ec76de3253db9294e8415794bcd8b26c
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-02-25 23:12:16 -0500

    Added ChampionDetailsActivityTest. Only testing the MenuItem with Robolectric for now. WIP

---
+++ app/src/test/java/com/leaguechampions/ui/championdetails/ChampionDetailsActivityTest.java
@@ -0,0 +1,47 @@
+package com.leaguechampions.ui.championdetails;
+
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+public class ChampionDetailsActivityTest {
+
+    private ChampionDetailsActivity activity;
+
+    @Before
+    public void setUp() {
+        Intent intent = ChampionDetailsActivity.getIntent(RuntimeEnvironment.application, "7.15.1", "Riven");
+
+        activity = Robolectric.buildActivity(ChampionDetailsActivity.class, intent)
+                .create()
+//                .start()
+//                .resume()
+                .visible()
+                .get();
+    }
+
+    @Test
+    public void testOnOptionsItemSelected_WhenItemIsHome_ShouldFinish() {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        shadowActivity.clickMenuItem(android.R.id.home);
+        assertTrue(shadowActivity.isFinishing());
+    }
+
+    @Test
+    public void testOnOptionsItemSelected_WhenItemIsNotHome_ShouldNotFinish() {
+        ShadowActivity shadowActivity = shadowOf(activity);
+        shadowActivity.clickMenuItem(0);
+        assertFalse(shadowActivity.isFinishing());
+    }
+}
