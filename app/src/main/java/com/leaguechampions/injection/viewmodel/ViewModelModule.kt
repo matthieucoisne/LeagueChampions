3b7d85f9ba9cbc044f736b4f9df21ae7666ac035
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-09 00:25:21 -0500

    Added ViewModelModule. WIP

---
+++ app/src/main/java/com/leaguechampions/injection/viewmodel/ViewModelModule.kt
@@ -0,0 +1,25 @@
+package com.leaguechampions.injection.viewmodel
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.leaguechampions.ui.championdetails.ChampionDetailsViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+
+@Module
+abstract class ViewModelModule {
+//    @Binds
+//    @IntoMap
+//    @ViewModelKey(ChampionsViewModel::class)
+//    internal abstract fun bindChampionsViewModel(championsViewModel: ChampionsViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ChampionDetailsViewModel::class)
+    internal abstract fun bindChampionDetailsViewModel(championDetailsViewModel: ChampionDetailsViewModel): ViewModel
+
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
