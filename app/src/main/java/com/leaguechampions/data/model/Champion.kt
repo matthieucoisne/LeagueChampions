c1bc8b55150c2892ef527928b9282c86bb3e4b9e
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-11 17:31:50 -0400

    ChampionDetails now uses databinding

--- app/src/main/java/com/leaguechampions/data/model/Champion.kt
+++ app/src/main/java/com/leaguechampions/data/model/Champion.kt
@@ -1,10 +1,22 @@
 package com.leaguechampions.data.model
 
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+
 data class Champion(
         val id: String,
         val name: String,
         val title: String,
-        val lore: String) : Comparable<Champion> {
+        private val lore: String) : Comparable<Champion> {
+
+    fun getLore(): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(lore, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(lore)
+        }
+    }
 
     override fun compareTo(other: Champion): Int {
         return name.compareTo(other.name)
--- app/src/main/java/com/leaguechampions/ui/championdetails/ChampionDetailsActivity.kt
+++ app/src/main/java/com/leaguechampions/ui/championdetails/ChampionDetailsActivity.kt
@@ -4,33 +4,22 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
-import android.text.Html
 import android.view.MenuItem
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
 import com.leaguechampions.R
 import com.leaguechampions.data.local.Const
-import com.leaguechampions.data.model.Champion
+import com.leaguechampions.databinding.ActivityChampionDetailsBinding
 import com.leaguechampions.injection.viewmodel.ViewModelFactory
-import com.leaguechampions.utils.UrlUtils
-import com.squareup.picasso.Picasso
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class ChampionDetailsActivity : DaggerAppCompatActivity() {
 
-    private lateinit var toolbar: Toolbar
-    private lateinit var ivChampion: ImageView
-    private lateinit var tvName: TextView
-    private lateinit var tvLore: TextView
-
+    private lateinit var binding: ActivityChampionDetailsBinding
     private lateinit var viewModel: ChampionDetailsViewModel
 
     @Inject lateinit var viewModelFactory: ViewModelFactory
-    @Inject lateinit var picasso: Picasso
 
     companion object {
         fun getIntent(context: Context, championId: String): Intent {
@@ -42,24 +31,19 @@ class ChampionDetailsActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_champion_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_champion_details)
 
-        toolbar = findViewById(R.id.activity_champion_details_toolbar)
-        ivChampion = findViewById(R.id.activity_champion_details_ivChampion)
-        tvName = findViewById(R.id.activity_champion_details_tvName)
-        tvLore = findViewById(R.id.activity_champion_details_tvLore)
-
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.activityChampionDetailsToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.app_name)
 
         val championId = intent.getStringExtra(Const.KEY_CHAMPION_ID)
 
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(ChampionDetailsViewModel::class.java)
-        viewModel.setChampionId(championId)
         viewModel.riotResponse.observe(this, Observer { riotResponseResource ->
-            showDetails(riotResponseResource?.data?.data?.get(championId))
+            binding.champion = riotResponseResource?.data?.data?.get(championId)
         })
+        viewModel.setChampionId(championId)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -70,17 +54,6 @@ class ChampionDetailsActivity : DaggerAppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun showDetails(champion: Champion?) {
-        if (Const.isGlide) {
-            Glide.with(this).load(UrlUtils.getImageUrl(this, "7.15.1", champion?.id!!)).into(ivChampion)
-        } else {
-            picasso.load(UrlUtils.getImageUrl(this, "7.15.1", champion?.id!!)).into(ivChampion)
-        }
-
-        tvName.text = champion?.name
-        tvLore.text = Html.fromHtml(champion?.lore)
     }
 
 //    override fun showError(@StringRes stringId: Int) {
