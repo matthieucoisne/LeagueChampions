be45359cfff8784e9844cfdefbe26cb951d14326
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-15 00:59:24 -0400

    Added RiotRealm in model to get the latest versions from Riot

---
+++ app/src/main/java/com/leaguechampions/data/model/RiotRealm.kt
@@ -0,0 +1,15 @@
+package com.leaguechampions.data.model
+
+import com.google.gson.annotations.SerializedName
+
+class RiotRealm(@SerializedName("n")
+                private val version: RiotVersion) {
+
+    fun getVersion(): String {
+        return version.champion
+    }
+
+    class RiotVersion {
+        internal lateinit var champion: String
+    }
+}
