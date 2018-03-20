90b99253de428c34daf302c46afcad4e2153bf39
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-19 22:10:32 -0400

    fixed classes in release folder

--- app/src/release/java/com/leaguechampions/utils/UrlUtils.java
+++ app/src/release/java/com/leaguechampions/utils/UrlUtils.java
@@ -2,11 +2,14 @@ package com.leaguechampions.utils;
 
 import android.content.Context;
 
+import com.leaguechampions.data.local.Const;
+import com.leaguechampions.data.model.Champion;
+
 public final class UrlUtils {
 
     private UrlUtils() {}
 
-    public static String getImageUrl(Context context, String version, String championKey) {
-        return "http://ddragon.leagueoflegends.com/cdn/"+version+"/img/champion/"+championKey+".png";
+    public static String getImageUrl(Context context, Champion champion) {
+        return Const.URL_BASE + "cdn/" + champion.getVersion() + "/img/champion/" + champion.getId() + ".png";
     }
 }
