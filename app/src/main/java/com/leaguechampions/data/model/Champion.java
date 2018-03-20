64d0ee4064cbad4adcef62c2ad6d64557aa9960b
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-19 21:51:24 -0400

    Added a version field to Champion

--- app/src/main/java/com/leaguechampions/data/model/Champion.java
+++ app/src/main/java/com/leaguechampions/data/model/Champion.java
@@ -1,12 +1,16 @@
 package com.leaguechampions.data.model;
 
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.text.Html;
+import android.text.Spanned;
 
 public class Champion implements Comparable<Champion> {
 
     private String id;
     private String name;
     private String title;
+    private String version;
     private String lore;
 
     public Champion(String name) {
@@ -25,12 +29,21 @@ public class Champion implements Comparable<Champion> {
         return title;
     }
 
-    public String getLore() {
-        return lore;
+    public String getVersion() {
+        return version;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @SuppressWarnings("deprecation")
+    public Spanned getLore() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(lore, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            return Html.fromHtml(lore);
+        }
     }
 
     @Override
