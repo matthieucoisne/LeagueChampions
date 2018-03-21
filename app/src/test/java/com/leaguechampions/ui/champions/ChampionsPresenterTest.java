3fa4ae05cdf1792cc86cda0f1ad5d89d3227cb89
Matthieu Coisne <matthieu.coisne@gmail.com>
2018-03-20 23:28:29 -0400

    Fixed ChampionDetailsPresenterTest

--- app/src/test/java/com/leaguechampions/ui/champions/ChampionsPresenterTest.java
+++ app/src/test/java/com/leaguechampions/ui/champions/ChampionsPresenterTest.java
@@ -5,29 +5,32 @@ import android.view.MenuItem;
 
 import com.leaguechampions.R;
 import com.leaguechampions.data.model.Champion;
+import com.leaguechampions.data.model.RiotRealm;
 import com.leaguechampions.data.model.RiotResponse;
 import com.leaguechampions.data.remote.Api;
+import com.leaguechampions.utils.ImmediateSchedulersRule;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.reactivex.Observable;
 import okhttp3.MediaType;
 import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
+import retrofit2.HttpException;
 import retrofit2.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -38,23 +41,27 @@ public class ChampionsPresenterTest {
 
     private ChampionsPresenter presenter;
 
-    private Map<String, Champion> data = new HashMap<String, Champion>() {{
+    private final Map<String, Champion> data = new HashMap<String, Champion>() {{
         put("Riven", new Champion("Riven"));
         put("Jinx", new Champion("Jinx"));
     }};
 
+    private final HttpException httpException = new HttpException(Response.error(400, ResponseBody.create(MediaType.parse("application/json"), "{\"error\":\"failure\"}")));
+    private final IOException ioException = new IOException();
+
     @Mock ChampionsPresenter.ChampionsView view;
     @Mock Api api;
-    @Mock Call<RiotResponse> call;
-    @Mock RiotResponse riotResponse;
 
-    @Captor ArgumentCaptor<Callback<RiotResponse>> argumentCaptor;
+    @Mock RiotResponse riotResponse;
+    @Mock RiotRealm riotRealm;
+
+    @Rule ImmediateSchedulersRule rule = new ImmediateSchedulersRule();
 
     @Before
     public void setUp() {
         presenter = new ChampionsPresenter(view, api);
 
-        when(api.getChampions()).thenReturn(call);
+        when(riotRealm.getVersion()).thenReturn("1.0.0");
         when(riotResponse.getData()).thenReturn(data);
     }
 
@@ -66,56 +73,58 @@ public class ChampionsPresenterTest {
     @Test
     public void testOnActivityCreated() {
         Bundle arguments = mock(Bundle.class);
+        when(api.getVersion()).thenReturn(Observable.just(riotRealm));
+        when(api.getChampions(anyString())).thenReturn(Observable.just(riotResponse));
 
         presenter.onActivityCreated(null, arguments);
 
-        verify(api.getChampions()).enqueue(argumentCaptor.capture());
-        argumentCaptor.getValue().onResponse(call,
-                Response.success(riotResponse)
-        );
-        verify(view).setAdapter(riotResponse);
+        verify(view).setAdapter(new ArrayList<>(data.values()));
         verifyNoMoreInteractions(view);
     }
 
     @Test
-    public void testOnActivityCreated_WhenResponseError400_ShowsError() throws Exception {
+    public void testOnActivityCreated_WhenGetVersionThrowsError400_ShowsError() throws Exception {
         Bundle arguments = mock(Bundle.class);
+        when(api.getVersion()).thenReturn(Observable.error(httpException));
 
         presenter.onActivityCreated(null, arguments);
 
-        verify(api.getChampions()).enqueue(argumentCaptor.capture());
-        argumentCaptor.getValue().onResponse(call,
-                Response.<RiotResponse>error(400, ResponseBody.create(MediaType.parse("application/json"), "{\"error\":\"failure\"}"))
-        );
-        verify(view).showError(R.string.error_code, 400);
+        verify(view).showError(R.string.error_something_went_wrong);
         verifyNoMoreInteractions(view);
     }
 
     @Test
-    public void testOnActivityCreated_WhenIOFailure_ShowsError() throws Exception {
+    public void testOnActivityCreated_WhenGetChampionsThrowsError400_ShowsError() throws Exception {
         Bundle arguments = mock(Bundle.class);
+        when(api.getVersion()).thenReturn(Observable.just(riotRealm));
+        when(api.getChampions(anyString())).thenReturn(Observable.error(httpException));
 
         presenter.onActivityCreated(null, arguments);
 
-        verify(api.getChampions()).enqueue(argumentCaptor.capture());
-        argumentCaptor.getValue().onFailure(call,
-                new IOException()
-        );
+        verify(view).showError(R.string.error_something_went_wrong);
+        verifyNoMoreInteractions(view);
+    }
+
+    @Test
+    public void testOnActivityCreated_WhenGetVersionThrowsIOError_ShowsError() throws Exception {
+        Bundle arguments = mock(Bundle.class);
+        when(api.getVersion()).thenReturn(Observable.error(ioException));
+
+        presenter.onActivityCreated(null, arguments);
+
         verify(view).showError(R.string.error_io);
         verifyNoMoreInteractions(view);
     }
 
     @Test
-    public void testOnActivityCreated_WhenFailure_ShowsError() throws Exception {
+    public void testOnActivityCreated_WhenGetChampionsThrowsIOError_ShowsError() throws Exception {
         Bundle arguments = mock(Bundle.class);
+        when(api.getVersion()).thenReturn(Observable.just(riotRealm));
+        when(api.getChampions(anyString())).thenReturn(Observable.error(ioException));
 
         presenter.onActivityCreated(null, arguments);
 
-        verify(api.getChampions()).enqueue(argumentCaptor.capture());
-        argumentCaptor.getValue().onFailure(call,
-                new Throwable()
-        );
-        verify(view).showError(R.string.error_something_went_wrong);
+        verify(view).showError(R.string.error_io);
         verifyNoMoreInteractions(view);
     }
 
@@ -144,14 +153,13 @@ public class ChampionsPresenterTest {
 
     @Test
     public void testOnItemClick() {
-        String version = "7.15.1";
         String championId = "Riven";
         Champion champion = mock(Champion.class);
         when(champion.getId()).thenReturn(championId);
 
-        presenter.onItemClick(version, champion);
+        presenter.onItemClick(champion);
 
-        verify(view).showDetails(version, championId);
+        verify(view).showDetails(championId);
         verifyNoMoreInteractions(view);
     }
 }
